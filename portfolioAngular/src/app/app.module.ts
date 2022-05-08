74f548edf2b28d79f103c4a832b1c8a7da2019e3
GSC996 <gscoutinho@outlook.com>
2022-05-08 13:00:22 -0300

    creacion de componentes a partir de la maqueta

--- portfolioAngular/src/app/app.module.ts
+++ portfolioAngular/src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { AcercaDeComponent } from './componets/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './componets/experiencia/experiencia.component';
+import { EducacionComponent } from './componets/educacion/educacion.component';
+import { SkillsComponent } from './componets/skills/skills.component';
+import { ProyectosRealizadosComponent } from './componets/proyectos-realizados/proyectos-realizados.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosRealizadosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ portfolioAngular/src/app/componets/proyectos-realizados/proyectos-realizados.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProyectosRealizadosComponent } from './proyectos-realizados.component';
+
+describe('ProyectosRealizadosComponent', () => {
+  let component: ProyectosRealizadosComponent;
+  let fixture: ComponentFixture<ProyectosRealizadosComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProyectosRealizadosComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProyectosRealizadosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
